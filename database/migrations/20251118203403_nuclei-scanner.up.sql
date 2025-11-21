b35f50f77f1315d0ff136d24c867fdb91f3b872a
Steffen Schön <steffen1995@gmail.com>
2025-11-21 19:21:34 +0100

    added nuclei scanner config to database

---
+++ database/migrations/20251118203403_nuclei-scanner.up.sql
@@ -0,0 +1,2 @@
+-- insert default nuclei scanner config
+insert into scan_configs (id, name, type, engine) values ('9a95d1de-b839-4e09-9837-921075e0c8bd', 'Nuclei Default', 'vulnerability', 'nuclei');
